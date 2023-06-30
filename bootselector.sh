c0409b019c9e45be20f07f8e2dac605db6f9d969
FarWrong <morewrong32@gmail.com>
2023-06-29 23:27:46 -0400

    custom module and custom commit

---
+++ bootselector.sh
@@ -0,0 +1,13 @@
+KERNELVER=5.15.108+
+MID=$(awk '/Advanced options for Ubuntu/{print $(NF-1)}' /boot/grub/grub.cfg | cut -d\' -f2)
+KID=$(awk "/with Linux $KERNELVER/"'{print $(NF-1)}' /boot/grub/grub.cfg | cut -d\' -f2 | head -n1)
+
+# echo "GRUB_DEFAULT='${MID}>${KID}'" >> /etc/default/grub
+
+# or prefer put it in a seprate file
+cat > /etc/default/grub.d/95-savedef.cfg <<EOF
+GRUB_DEFAULT=saved
+GRUB_SAVEDEFAULT=true
+EOF
+grub-editenv /boot/grub/grubenv set saved_entry="${MID}>${KID}"
+update-grub
